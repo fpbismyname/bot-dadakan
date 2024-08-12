e81735eef3b307cafaf423bfe1877cce032cd4cd
fpbismyname <fajarpebefajar12@gmail.com>
2024-08-12 13:36:53 +0700

    this crazy

--- server.js
+++ server.js
@@ -1,44 +1,57 @@
 const mineflayer = require('mineflayer');
-// const keep_alive = require('./keep_alive');
+const dotenv = require('dotenv')
 
-var connects = 0
-try{
-    const connect = () =>{
-        connects = connects + 1
-        const bot = mineflayer.createBot({
-            host: 'server_dadakan.aternos.me', // Ganti dengan IP server Aternos kamu
-            port:  44671,                    // Port default Minecraft, ganti jika berbeda
-            username: 'Kuncen_Dadakan',            // Nama pengguna bot
-            version: '1.20'               // Versi Minecraft yang sama dengan server kamu
-        });
-        // bot.on('spawnReset', ()=>{
-        //     console.log('Bot telah respawn')
-        // })
-        bot.on('spawn', () => {
-            console.log('Bot has spawned in the game.');
-        });
-        
-        bot.on('error', err => {
-            console.error('Error:', err);
-        });
-        
-        bot.on('end', () => {
-            console.clear()
-            console.log('Bot telah mati.' + connects);
-        });
-        bot.on('death', ()=>{
-            bot.respawn()
-            connect()
-        })
-        bot.on('playerLeft', ()=>{
-            connect()
-        })
-        bot.on('kicked', ()=>{
-            connect()
-        })
+
+
+try {
+    dotenv.config()
+} catch (err) {
+    console.log(err)
+} finally {
+    // const keep_alive = require('./keep_alive');
+
+    var connects = 0
+    try {
+        const connect = () => {
+            connects = connects + 1
+            const bot = mineflayer.createBot({
+                host: process.env.SERVER_ATERNOS, // Ganti dengan IP server Aternos kamu
+                port: process.env.PORT_SERVER,                    // Port default Minecraft, ganti jika berbeda
+                username: process.env.BOT_NAME,            // Nama pengguna bot
+                version: process.env.MINECRAFT_VERSION               // Versi Minecraft yang sama dengan server kamu
+            });
+            // bot.on('spawnReset', ()=>{
+            //     console.log('Bot telah respawn')
+            // })
+            bot.on('spawn', () => {
+                console.log('Bot has spawned in the game.');
+            });
+
+            bot.on('error', err => {
+                console.error('Error:', err);
+            });
+
+            bot.on('end', () => {
+                console.clear()
+                console.log('Bot telah mati.' + connects);
+                setTimeout(()=>{
+                    connect()
+                },1000)
+            });
+            bot.on('death', () => {
+                bot.respawn()
+                connect()
+            })
+            bot.on('playerLeft', () => {
+                connect()
+            })
+            bot.on('kicked', () => {
+                connect()
+            })
+        }
+        connect()
+        console.clear()
+    } catch (err) {
+        console.error('Error:', err);
     }
-    connect()
-    console.clear()
-} catch (err){
-    console.error('Error:', err);
 }
